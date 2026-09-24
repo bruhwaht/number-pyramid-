c843c45e32ed4920f82e1e3bf479dc2d051f5010
Bruhwaht <nandithahari2008@gmail.com>
2026-09-24 16:26:57 +0530

    pyramidcreation

---
+++ number pyramid wala 1 121 32123.py
@@ -0,0 +1,26 @@
+x=int(input("enter number: "))
+
+
+y = x
+z = 1
+w = 1
+
+if x > 9:
+    print("Nah, I do not grant you PERMISSION to enter a number above 9!")
+
+else:
+    for i in range(x):
+        z = z + 1
+        y = y - 1
+
+        # Create spaces before each number
+        u = " " * y
+
+        # Print the current row
+        print(u + str(w))
+
+       # Build the next row by adding the new number
+       # to both sides of the previous row
+        w = str(z) + str(w) + str(z)
+
+    
